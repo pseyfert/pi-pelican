eee82d3acc672aa1e0d61ca0c980040fa52f14f7
Paul Seyfert <paul.seyfert@mib.infn.it>
2015-12-12 14:45:18 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pi-pelican
 a pelican theme to blend into the webpages of the physics institute at heidelberg university
+
+# issues
+
+ * this certainly does not profit from the central php header and footer file and excludes virtually all the features shipped along
+ * i don't ship the university's css files
+ * the license disclaimer (required by the AGPL 3 license from the original 'simple' theme of pelican) is NOT PRESENT on the final page. MUST! be added when deploying!
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'INSERT NAME'
+SITENAME = u'INSERT NAME'
+SITEURL = 'file:///home/NAME/pelican-test/output'
+INDEX_SAVE_AS = 'blog_index.html'
+
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["render_math"]
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None#'feeds'
+
+
+DEFAULT_PAGINATION = 10
+
+RELATIVE_URLS = True
+
